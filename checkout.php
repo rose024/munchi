7163bd21d5ed997f79ce1848b912c7c5d24a6233
Rose Rabano <115044957+rose024@users.noreply.github.com>
2023-12-15 10:58:42 -0800

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -1,16 +1,15 @@
 <?php include 'partials/head.php'; ?>
  
  <body>
-      <!--linked the header into a partial-->
+   
      <?php include 'partials/header.php'; ?>
      <div id="container">
          
          <div class="box">
      
-         
-         <!--the reservation-->
- <a href="checkout.php">checkout</a>
-         <?php include 'partials/body.php'; ?>
+     
+ 
+         <?php include 'partials/checkout.php'; ?>
   
      </div></div>
      
