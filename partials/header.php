4083d86e9adb1b1b725a678a38c9d1a7f2af8d0b
Keitlyn L <78679013+keitlyn-lau@users.noreply.github.com>
2023-12-14 19:04:03 -0800

    Building structure

---
+++ partials/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Munchi | Snack Subscription Box | Japanese Snacks Collection</title>
+
+    <link type="text/css" rel="stylesheet" href="../assets/css/global.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <h1>Munchi</h1>
+                <img src="../assets/img/logo.png" alt="logo of munchi">
+            </div>
+            <div>
+                <div>
+                    <a href="">Menu</a>
+                    <a href="">Subscription</a>
+                    <a href="">Contact Us</a>
+                </div>
+                <div>
+
+                    <button class="btn-join-now">Join Now</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
