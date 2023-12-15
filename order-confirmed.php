ef04153c15b398edb3729f4336f7fe0da8e3eaf3
Keitlyn L <78679013+keitlyn-lau@users.noreply.github.com>
2023-12-14 23:08:52 -0800

    Order Confirmation page built

---
+++ order-confirmed.php
@@ -0,0 +1,94 @@
+<?php
+    include('partials/header.php');
+?>
+
+<main class="orders">
+
+    <div class="orders__container">
+
+        <div class="orders__container_title">
+            <h1>Congratulations</h1>
+            <img src="./assets/img/Asset 20.png" alt="">
+            <h2>Your Order Is Confirmed!</h2>
+        </div>
+
+        <div class="orders__container_card">
+
+            <div class="orders__container_card_section1">
+                <div>
+                    <h3>Order ID: A8795123467</h3>
+                </div>
+                <div class="flex-row-space-between">
+                    <p>Order date: Dec 9, 2022</p>
+                    <p>Expected Delivery: Dec 24, 2023</p>
+                </div>
+            </div><hr>
+
+            <div class="orders__container_card_section2 flex-row">
+                <div>
+                    <img src="./assets/img/7.jpg" alt="" style="width: 160px; margin-right: 55px;">
+                </div>
+                <div>
+                    <h4>Selected Option</h4>
+                    <div>
+                        <p>For Myself</p>
+                        <p>For Individual</p>
+                        <p>Bi-weekly</p>
+                        <p>Monthly Billing</p>
+                        <p>Far East Asia</p>
+                    </div>
+                </div>
+            </div><hr>
+
+            <div class="orders__container_card_section3">
+                <div>
+                    <h4>Payment</h4>
+                    <p>Visa **56</p>
+                </div>
+                <div>
+                    <h4>Delivery</h4>
+                    <p>Address</p>
+                    <p>847 Jewess Bridge Apt.</p>
+                    <p>174 London, UK</p>
+                    <p>474-769-3919</p>
+                </div>
+            </div><hr>
+
+            <div class="orders__container_card_section4">
+                <h4>Order Summary</h4>
+                <div class="flex-row-space-between">
+                    <div>Discount</div>
+                    <div>100</div>
+                </div>
+                <div class="flex-row-space-between">
+                    <div>Discount</div>
+                    <div>(10%)-$00.00</div>
+                </div>
+                <div class="flex-row-space-between">
+                    <div>Shipping</div>
+                    <div>$00.00</div>
+                </div>
+                <div class="flex-row-space-between">
+                    <div>Tax</div>
+                    <div>+$00.00</div>
+                </div>
+                <hr>
+                <div class="flex-row-space-between">
+                    <div>Total</div>
+                    <div>$000.00</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="orders__container_buttons">
+            <img src="./assets/img/Asset 12.png" alt="">
+        </div>
+
+    </div>
+
+</main>
+
+<?php
+    include('partials/footer.php');
+?>
