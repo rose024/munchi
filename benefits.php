78178e4001f0d0ab352f66baaf38fd8eaba57262
Keitlyn L <78679013+keitlyn-lau@users.noreply.github.com>
2023-12-14 22:40:27 -0800

    Benefits page built

---
+++ benefits.php
@@ -0,0 +1,51 @@
+<?php
+    include('partials/header.php');
+?>
+
+<main class="benefits">
+
+    <div class="benefits__container">
+
+        <div class="hero_banner">
+            <div class="promotion_banner"><img src="./assets/img/munch-promotion-banner.webp" alt=""></div>
+            <img src="./assets/img/bg.jpg" alt="">
+        </div>
+
+        <div class="benefits__container_content">
+
+            <div>
+                <h1>Benefits of Munchi Memberships</h1>
+            </div>
+
+            <div class="flex-row">
+                <div class="benefits__container_section">
+                    <img src="./assets/img/6.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Munchi imports tasty snacks from other regions of the world in order for you to try new snacking adventures.</p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/3.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Regardless of your subscription type you can cancel at any time without any fees or penalties.</p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/4.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>We listen! If you have any suggestions to give us about your snacking preferences we will try our best to fulfill them. </p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/5.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>In time we plan to import great snacks from more regions of the world so that we can continue to give our members more options to choose from.</p>
+                </div>
+            </div>
+
+            <div class="benefits__container_buttons">
+                <img src="./assets/img/Asset 13.png" alt="">
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
+
+<?php
+    include('partials/footer.php');
+?>
