3fb984a9394e0bd7e595c4b71bf0552d0ccf5d35
Rose Rabano <115044957+rose024@users.noreply.github.com>
2023-12-15 11:43:44 -0800

    Update index.php
    updated index php

--- index.php
+++ index.php
@@ -1,20 +1,54 @@
-<?php include 'partials/head.php'; ?>
- 
- <body>
-      <!--linked the header into a partial-->
-     <?php include 'partials/header.php'; ?>
-     <div id="container">
-         
-         <div class="box">
-     
-         
-         <!--the reservation-->
- <a href="checkout.php">checkout</a>
-         <?php include 'partials/body.php'; ?>
-  
-     </div></div>
-     
-    <!-- footer -->
-    
- </body>
- </html>
+<?php
+    include('partials/header.php');
+?>
+
+<main class="benefits">
+
+    <div class="benefits__container">
+
+        <div class="hero_banner">
+            <div class="promotion_banner"><img src="./assets/img/munch-promotion-banner.webp" alt=""></div>
+            <img src="./assets/img/bg.jpg" alt="">
+        </div>
+
+        <div class="benefits__container_content">
+
+            <div>
+                <h1> How Munchi Works</h1>
+            </div>
+
+            <div class="flex-row">
+                <div class="benefits__container_section">
+                    <img src="./assets/img/6.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Your first step is to choose which size of snack box you want to order. Small, medium, or large?</p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/3.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Then decide if you want to subscribe for a prepaid period of one year (which saves you money) or to subscribe monthly.</p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/4.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Next you choose which type of snack box you want to order. Which region appeals to you most?  </p>
+                </div>
+                <div class="benefits__container_section">
+                    <img src="./assets/img/5.jpg" alt="" class="benefits__container_thumbnails">
+                    <p>Now you only need to fill out your payment information along with your address. Don’t forget...your first order is 20% off!</p>
+                </div>
+            </div>
+
+            <div class="benefits__container_buttons">
+                <a href="subscription.php">
+                    <img src="./assets/img/Asset 13.png" alt="">
+                </a>
+                
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
+
+<?php
+    include('partials/footer.php');
+?>
