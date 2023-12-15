a64217d0e2714574b3b39722d893ae3e8e63f3f1
Keitlyn L <78679013+keitlyn-lau@users.noreply.github.com>
2023-12-14 22:22:11 -0800

    Initialize benefits page

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
     include('partials/header.php');
+?>
 
+<main class="landing">
+
+    <div class="landing__container">
+
+        <div class="hero_banner">
+            <div class="promotion_banner"><img src="./assets/img/munch-promotion-banner.webp" alt=""></div>
+            <img src="./assets/img/bg.jpg" alt="">
+        </div>
+
+        <div class="landing__container_benefits">
+            <h1>Benefits of Munchi Memberships</h1>
+        </div>
+
+    </div>
+
+</main>
+
+<?php
     include('partials/footer.php');
 ?>
