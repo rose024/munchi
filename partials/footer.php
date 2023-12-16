d28902e481053985cd7d5f649e0d4d0a961da71f
Rose Rabano <115044957+rose024@users.noreply.github.com>
2023-12-15 19:27:54 -0800

    fixed bug #4

--- partials/footer.php
+++ partials/footer.php
@@ -9,10 +9,10 @@
             <p>&copy; 2023 Munchi. We love Munchinators!</p>
         </div>
         <div>
-            <a href=""><img src="../assets/img/twitter.svg" alt="Munchi's X channel"></a>
-            <a href=""><img src="../assets/img/ig.svg" alt="Munchi's Instagram channel"></a>
-            <a href=""><img src="../assets/img/fb.svg" alt="Munchi's Facebook channel"></a>
-            <a href=""><img src="../assets/img/tiktok.svg" alt="Munchi's Tiktok channel"></a>
+            <a href="https://twitter.com/home?lang=en"><img src="../assets/img/twitter.svg" alt="Munchi's X channel"></a>
+            <a href="https://www.instagram.com/"><img src="../assets/img/ig.svg" alt="Munchi's Instagram channel"></a>
+            <a href="https://www.facebook.com/"><img src="../assets/img/fb.svg" alt="Munchi's Facebook channel"></a>
+            <a href="https://www.tiktok.com/"><img src="../assets/img/tiktok.svg" alt="Munchi's Tiktok channel"></a>
         </div>
     </footer>
 </body>
